b91e78999c0bbb17dca795239e58c4aaeecb02b8
Anoop Kumar Sharma <anoopkrsh22@gmail.com>
2023-10-06 15:13:54 +0530

    Update TransformChilds.cs

--- HelperFunctions/Utilities/TransformChilds.cs
+++ HelperFunctions/Utilities/TransformChilds.cs
@@ -9,4 +9,17 @@ namespace HelperFunction.HelperFunctions.Utilities
             foreach (Transform child in t) Object.Destroy(child.gameObject);
         }
     }
+
+    public static class GetMainParent
+    {
+        public static Transform FindParent(this Transform t)
+        {
+            var temp = t;
+            while (true)
+            {
+                if (temp.parent == null) return temp;
+                temp = temp.parent;
+            }
+        }
+    }
 }
